dbd6699c2e80f762bfd769f1611d5b87bd298dad
aucorp <abid.hossain@pmamediagroup.com>
2023-03-13 14:31:54 +0600

    layout added with routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Main from "./layout/Main";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to={"/home"} replace />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </Router>
+      </Main>
+    </div>
+  );
 }
 
-export default App
+export default App;
